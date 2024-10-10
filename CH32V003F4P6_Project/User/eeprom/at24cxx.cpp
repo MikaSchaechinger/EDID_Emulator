b3ab69a83c8e827f60d30478e5083b365d532323
Mika Schächinger <74455957+MikaSchaechinger@users.noreply.github.com>
2024-10-10 15:45:30 +0200

    Added Timeout and NACK Error to I2C COmmunication

--- CH32V003F4P6_Project/User/eeprom/at24cxx.cpp
+++ CH32V003F4P6_Project/User/eeprom/at24cxx.cpp
@@ -19,92 +19,73 @@ uint8_t AT24CXX::getI2CAddress() {
 }
 
 
-uint8_t AT24CXX::readOneByte(uint16_t addr)
-{
-    uint8_t temp = 0;
-    // Wait until the I2C bus is no longer busy
-    while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY) != RESET);
-    // Generate a START condition
-    I2C_GenerateSTART(I2C1, ENABLE);
-    // Wait until START condition has been transmitted
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
-    // Send EEPROM address (use the class member 'address') in write mode (0xA0)
-    I2C_Send7bitAddress(I2C1, this->address, I2C_Direction_Transmitter);
-    // Wait until address has been acknowledged
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-    // Send the memory address to read from
-    I2C_SendData(I2C1, (uint8_t)(addr & 0x00FF));  // Send the LSB of the memory address
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-    // Generate a RESTART condition to switch to read mode
-    I2C_GenerateSTART(I2C1, ENABLE);
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
-    // Send EEPROM address in read mode (0xA1)
-    I2C_Send7bitAddress(I2C1, this->address, I2C_Direction_Receiver);
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
-    // Wait until the byte has been received
-    while (I2C_GetFlagStatus(I2C1, I2C_FLAG_RXNE) == RESET);
-    // Disable ACK as we are reading only one byte
-    I2C_AcknowledgeConfig(I2C1, DISABLE);
-    // Read the received byte
-    temp = I2C_ReceiveData(I2C1);
-    // Generate a STOP condition to end the transmission
-    I2C_GenerateSTOP(I2C1, ENABLE);
-    // Return the received byte
-    return temp;
+bool AT24CXX::waitForI2CEvent(uint32_t event) {
+    uint32_t timeout = TIMEOUT_LIMIT;
+    while (!I2C_CheckEvent(I2C1, event)) {
+        if (timeout-- == 0) {
+            return false;   // Timeout occurred
+        }
+    }
+    return true;
 }
 
 
-COMMUNICATION_ERROR AT24CXX::writeOneByte(uint16_t addr, uint8_t data)
-{
-    // Wait until the I2C bus is no longer busy
-    while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY) != RESET);
-    // Generate a START condition
-    I2C_GenerateSTART(I2C1, ENABLE);
-    // Wait until START condition has been transmitted
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
-    // Send EEPROM address (use the class member 'address') in write mode
-    I2C_Send7bitAddress(I2C1, this->address, I2C_Direction_Transmitter);
-    // Wait until address has been acknowledged
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-    // Send the memory address to write to
-    I2C_SendData(I2C1, (uint8_t)(addr & 0x00FF));  // Send the LSB of the memory address
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-    // Send the data to be written
-    I2C_SendData(I2C1, data);
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-    // Generate a STOP condition to end the transmission
-    I2C_GenerateSTOP(I2C1, ENABLE);
-}
+
+
 
 
 COMMUNICATION_ERROR AT24CXX::readBytes(uint16_t addr, uint8_t* buffer, uint16_t length)
 {
     // Wait until the I2C bus is no longer busy
-    while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY) != RESET);
-
+    //while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY) != RESET);
+    uint32_t timeout = TIMEOUT_LIMIT;
+    while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY) != RESET) {
+        if (timeout-- == 0) {
+            return BUSY_ERROR;   // Bus is busy for too long
+        }
+    }
+    
     // Generate a START condition
     I2C_GenerateSTART(I2C1, ENABLE);
 
     // Wait until START condition has been transmitted
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
-
+    if (!this->waitForI2CEvent(I2C_EVENT_MASTER_MODE_SELECT)) {
+        return TIMEOUT_ERROR;   // Timeout occurred
+    }
     // Send EEPROM I2C address (already shifted) in write mode to set memory address
     I2C_Send7bitAddress(I2C1, this->address, I2C_Direction_Transmitter);
 
     // Wait until address has been acknowledged
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-
+    if (!this->waitForI2CEvent(I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) {
+        if (I2C_GetFlagStatus(I2C1, I2C_FLAG_AF) == SET) {  // Check if NACK received   
+            I2C_ClearFlag(I2C1, I2C_FLAG_AF);
+            return NACK_ERROR;   // NACK received
+        }
+        return TIMEOUT_ERROR;   // Timeout occurred
+    }
     // Send the memory address to read from
     I2C_SendData(I2C1, (uint8_t)(addr & 0x00FF));  // Send the LSB of the memory address
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-
+    if (!this->waitForI2CEvent(I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {
+        if (I2C_GetFlagStatus(I2C1, I2C_FLAG_AF) == SET) {  // Check if NACK received   
+            I2C_ClearFlag(I2C1, I2C_FLAG_AF);
+            return NACK_ERROR;   // NACK received
+        }
+        return TIMEOUT_ERROR;   // Timeout occurred
+    }
     // Generate a RESTART condition to switch to read mode
     I2C_GenerateSTART(I2C1, ENABLE);
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
-
+    if (!this->waitForI2CEvent(I2C_EVENT_MASTER_MODE_SELECT)) {
+        return TIMEOUT_ERROR;   // Timeout occurred
+    }
     // Send EEPROM I2C address in read mode (already shifted with LSB for read mode)
     I2C_Send7bitAddress(I2C1, this->address | 1, I2C_Direction_Receiver);
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+    if (!this->waitForI2CEvent(I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED)) {
+        if (I2C_GetFlagStatus(I2C1, I2C_FLAG_AF) == SET) {  // Check if NACK received   
+            I2C_ClearFlag(I2C1, I2C_FLAG_AF);   // TODO: Necassary?
+            return NACK_ERROR;   // NACK received
+        }
+        return TIMEOUT_ERROR;   // Timeout occurred
+    }
 
     // Read multiple bytes
     while (length)
@@ -116,8 +97,13 @@ COMMUNICATION_ERROR AT24CXX::readBytes(uint16_t addr, uint8_t* buffer, uint16_t 
         }
         
         // Wait until the byte has been received
-        while (I2C_GetFlagStatus(I2C1, I2C_FLAG_RXNE) == RESET);
-        
+        timeout = TIMEOUT_LIMIT;
+        while (I2C_GetFlagStatus(I2C1, I2C_FLAG_RXNE) == RESET) {
+            if (timeout-- == 0) {
+                return TIMEOUT_ERROR;   // Timeout occurred
+            }
+        }
+
         // Store the received byte in the buffer
         *buffer = I2C_ReceiveData(I2C1);
         buffer++;  // Increment the buffer pointer
@@ -132,37 +118,64 @@ COMMUNICATION_ERROR AT24CXX::readBytes(uint16_t addr, uint8_t* buffer, uint16_t 
 COMMUNICATION_ERROR AT24CXX::writeBytes(uint16_t addr, uint8_t* buffer, uint16_t length)
 {
     // Wait until the I2C bus is no longer busy
-    while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY) != RESET);
+    uint32_t timeout = TIMEOUT_LIMIT;
+    while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY) != RESET) {
+        if (timeout-- == 0) {
+            return BUSY_ERROR;   // Bus is busy for too long
+        }
+    }
 
     // Generate a START condition
     I2C_GenerateSTART(I2C1, ENABLE);
 
     // Wait until START condition has been transmitted
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
+    if (!this->waitForI2CEvent(I2C_EVENT_MASTER_MODE_SELECT)) {
+        return TIMEOUT_ERROR;   // Timeout occurred
+    }
 
     // Send EEPROM I2C address (already shifted) in write mode
     I2C_Send7bitAddress(I2C1, this->address, I2C_Direction_Transmitter);
 
     // Wait until address has been acknowledged
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+    if (!this->waitForI2CEvent(I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) {
+        if (I2C_GetFlagStatus(I2C1, I2C_FLAG_AF) == SET) {  // Check if NACK received   
+            I2C_ClearFlag(I2C1, I2C_FLAG_AF);  // Clear NACK flag
+            return NACK_ERROR;   // NACK received, no device responded
+        }
+        return TIMEOUT_ERROR;   // Timeout occurred
+    }
 
     // Send the starting memory address to write to
     I2C_SendData(I2C1, (uint8_t)(addr & 0x00FF));  // Send the LSB of the memory address
-    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+    if (!this->waitForI2CEvent(I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {
+        if (I2C_GetFlagStatus(I2C1, I2C_FLAG_AF) == SET) {  // Check if NACK received
+            I2C_ClearFlag(I2C1, I2C_FLAG_AF);  // Clear NACK flag
+            return NACK_ERROR;   // NACK received
+        }
+        return TIMEOUT_ERROR;   // Timeout occurred
+    }
 
     // Write multiple bytes to the EEPROM
-    while (length--)
-    {
+    while (length--) {
         I2C_SendData(I2C1, *buffer);  // Send the byte from the buffer
         buffer++;  // Increment the buffer pointer
 
         // Wait until the byte has been transmitted
-        while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+        if (!this->waitForI2CEvent(I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {
+            if (I2C_GetFlagStatus(I2C1, I2C_FLAG_AF) == SET) {  // Check if NACK received
+                I2C_ClearFlag(I2C1, I2C_FLAG_AF);  // Clear NACK flag
+                return NACK_ERROR;   // NACK received
+            }
+            return TIMEOUT_ERROR;   // Timeout occurred
+        }
     }
 
     // Generate a STOP condition to end the transmission
     I2C_GenerateSTOP(I2C1, ENABLE);
+
+    return SUCCESS;  // Communication was successful
 }
+
 
 
 
--- CH32V003F4P6_Project/User/eeprom/at24cxx.h
+++ CH32V003F4P6_Project/User/eeprom/at24cxx.h
@@ -9,8 +9,11 @@
 #define TIMEOUT_LIMIT 4800000  // Timeout limit for I2C communication
 
 typedef enum{
-    SUCCESS,
-    TIMEOUT_ERROR
+    SUCCESS,        // No error occurred
+    TIMEOUT_ERROR,  // Timeout occurred
+    NACK_ERROR,     // Not acknowledge error
+    BUSY_ERROR,     // Bus busy error
+    UNKNOWN_ERROR   // Unknown error
 } COMMUNICATION_ERROR;
 
 
@@ -20,6 +23,9 @@ class AT24CXX{
 private:
     uint8_t address;  // I2C address of the EEPROM, but shifted one bit to the left
     uint16_t size;  // Size of the EEPROM in bytes
+    
+    // Wait for an I2C event to occur. Returns true if the event occurred, false if a timeout occurred
+    bool waitForI2CEvent(uint32_t event);
 
 
 public: 
@@ -32,10 +38,6 @@ public:
     // Get the I2C address of the EEPROM (not shifted)
     uint8_t getI2CAddress();
     
-    uint8_t readOneByte(uint16_t addr);
-
-    COMMUNICATION_ERROR writeOneByte(uint16_t addr, uint8_t data);
-
     COMMUNICATION_ERROR readBytes(uint16_t addr, uint8_t* buffer, uint16_t length);
 
     COMMUNICATION_ERROR writeBytes(uint16_t addr, uint8_t* buffer, uint16_t length);
